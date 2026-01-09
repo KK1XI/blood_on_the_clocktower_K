9e5c8f4c917bed64433077d6f5a6e442135532b9
KK1XI <101323099+KK1XI@users.noreply.github.com>
2026-01-09 15:52:55 +0800

    技能描述更新

--- game_data.py
+++ game_data.py
@@ -117,7 +117,7 @@ SCRIPTS = {
                 {
                     "id": "mayor",
                     "name": "镇长",
-                    "ability": "如果只剩下3名玩家存活且没有玩家被处决，你的阵营获胜。如果你在夜晚将被杀死，可能会改为另一名玩家死亡。",
+                    "ability": "如果只剩下3名玩家存活且没有玩家被处决，你的阵营获胜。如果你在夜晚即将死亡，可能会有一名其他玩家代替你死亡。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -169,7 +169,7 @@ SCRIPTS = {
                 {
                     "id": "spy",
                     "name": "间谍",
-                    "ability": "每个夜晚，你会查看魔典。你可能被错误地识别为善良玩家。",
+                    "ability": "每个夜晚，你能查看魔典。你可能会被当作善良阵营、镇民角色或外来者角色，即使你已死亡。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 49
@@ -177,7 +177,7 @@ SCRIPTS = {
                 {
                     "id": "scarlet_woman",
                     "name": "红唇女郎",
-                    "ability": "当存活玩家还有5人或以上时，如果恶魔死亡，你成为恶魔。",
+                    "ability": "如果大于等于五名玩家存活时（旅行者不计算在内）恶魔死亡，你变成那个恶魔。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -185,7 +185,7 @@ SCRIPTS = {
                 {
                     "id": "baron",
                     "name": "男爵",
-                    "ability": "场上会额外多两名外来者。【设置时将两个镇民换成外来者】",
+                    "ability": "会有额外的外来者在场。[+2 外来者]",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0,
@@ -229,7 +229,7 @@ SCRIPTS = {
                 {
                     "id": "chambermaid",
                     "name": "侍女",
-                    "ability": "每个夜晚，选择两名玩家（不包括你自己）：你会得知他们之中有多少人今晚因自己的能力而被唤醒。",
+                    "ability": "每个夜晚，你要选择除你以外的两名存活的玩家：你会得知他们中有几人在当晚因其自身能力而被唤醒。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 50
@@ -237,7 +237,7 @@ SCRIPTS = {
                 {
                     "id": "exorcist",
                     "name": "驱魔人",
-                    "ability": "每个夜晚*，选择一名玩家（不能选择之前选过的）：如果你选择了恶魔，他今晚不能行动。",
+                    "ability": "每个夜晚*，你要选择一名玩家（与上个夜晚不同）：如果你选中了恶魔，他会得知你是驱魔人，但他当晚不会因其自身能力而被唤醒。",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 21
@@ -261,7 +261,7 @@ SCRIPTS = {
                 {
                     "id": "gossip",
                     "name": "造谣者",
-                    "ability": "每天白天一次，你可以公开做出一个声明。如果它是真的，今晚一名玩家会死亡。",
+                    "ability": "每个白天，你可以公开发表一个声明。如果该声明正确，在当晚会有一名玩家死亡。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 46,
@@ -270,7 +270,7 @@ SCRIPTS = {
                 {
                     "id": "courtier",
                     "name": "侍臣",
-                    "ability": "游戏中仅一次，在夜晚，选择一个角色：他喝醉三天三夜。",
+                    "ability": "每局游戏限一次，在夜晚时，你可以选择一个角色：如果该角色在场，该角色从当晚开始醉酒三天三夜。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 18
@@ -278,7 +278,7 @@ SCRIPTS = {
                 {
                     "id": "professor",
                     "name": "教授",
-                    "ability": "游戏中仅一次，在夜晚*，选择一名死亡玩家：如果他是镇民，他复活。",
+                    "ability": "每局游戏限一次，在夜晚时*，你可以选择一名死亡的玩家：如果他是镇民，你会将他起死回生。",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 43
@@ -286,7 +286,7 @@ SCRIPTS = {
                 {
                     "id": "minstrel",
                     "name": "吟游诗人",
-                    "ability": "当爪牙因处决而死亡时，所有其他玩家（除了旅行者）都会喝醉到明天黄昏。",
+                    "ability": "当一名爪牙死于处决时，除了你和旅行者以外的所有其他玩家醉酒直到明天黄昏。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -294,7 +294,7 @@ SCRIPTS = {
                 {
                     "id": "tea_lady",
                     "name": "茶艺师",
-                    "ability": "如果你两个存活的邻居都是善良的，他们无法死亡。",
+                    "ability": "如果与你邻近的两名存活的玩家是善良的，他们不会死亡。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -302,7 +302,7 @@ SCRIPTS = {
                 {
                     "id": "pacifist",
                     "name": "和平主义者",
-                    "ability": "如果善良玩家因处决而死亡，可能改为他存活。",
+                    "ability": "被处决的善良玩家可能不会死亡。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -329,7 +329,7 @@ SCRIPTS = {
                 {
                     "id": "moonchild",
                     "name": "月之子",
-                    "ability": "当你得知自己死亡时，你可以公开选择一名存活玩家。如果他是善良的，他死亡。",
+                    "ability": "当你得知你死亡时，你要公开选择一名存活的玩家。如果他是善良的，在当晚他会死亡。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 48,
@@ -338,7 +338,7 @@ SCRIPTS = {
                 {
                     "id": "goon",
                     "name": "莽夫",
-                    "ability": "每个夜晚，第一个选择你的玩家会喝醉到明天黄昏。你改变阵营为他的阵营。",
+                    "ability": "每个夜晚，首个使用其自身能力选择了你的玩家会醉酒直到下个黄昏。你会转变为他的阵营。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 4,
@@ -358,7 +358,7 @@ SCRIPTS = {
                 {
                     "id": "godfather",
                     "name": "教父",
-                    "ability": "如果在白天没有人死亡，你必须在今晚杀死一名外来者。【设置时添加一个外来者】",
+                    "ability": "在你的首个夜晚，你会得知有哪些外来者角色在场。如果有外来者在白天死亡，你会在当晚被唤醒并且你要选择一名玩家：他死亡。[-1或+1外来者]",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 40,
@@ -367,7 +367,7 @@ SCRIPTS = {
                 {
                     "id": "devils_advocate",
                     "name": "恶魔代言人",
-                    "ability": "每个夜晚，选择一名存活玩家（不能选择之前选过的）：如果他明天因处决而死亡，他不会死亡。",
+                    "ability": "每个夜晚，你要选择一名存活的玩家（与上个夜晚不同）：如果明天白天他被处决，他不会死亡。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 15
@@ -383,7 +383,7 @@ SCRIPTS = {
                 {
                     "id": "mastermind",
                     "name": "主谋",
-                    "ability": "如果恶魔因处决而死亡（不是今天处决的），在宣布游戏结束前再进行一天。如果一名镇民在那天被处决，邪恶获胜。",
+                    "ability": "如果恶魔因为死于处决而因此导致游戏结束时，再额外进行一个夜晚和一个白天。在那个白天如果有玩家被处决，他的阵营落败。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -393,7 +393,7 @@ SCRIPTS = {
                 {
                     "id": "zombuul",
                     "name": "僵怖",
-                    "ability": "每个夜晚*，如果没有人因你的能力死亡，选择一名玩家：他死亡。你第一次死亡时，你会活着但表现为已死亡。",
+                    "ability": "每个夜晚*，如果今天白天没有人死亡，你会被唤醒并要选择一名玩家：他死亡。当你首次死亡后，你仍存活，但会被当作死亡。",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 25
@@ -401,7 +401,7 @@ SCRIPTS = {
                 {
                     "id": "pukka",
                     "name": "普卡",
-                    "ability": "每个夜晚，选择一名玩家：他中毒。被你选中的前一个玩家会死亡然后恢复健康。",
+                    "ability": "每个夜晚，选择一名玩家：他中毒。上个因你的能力中毒的玩家会死亡并恢复健康。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 26
@@ -409,7 +409,7 @@ SCRIPTS = {
                 {
                     "id": "shabaloth",
                     "name": "沙巴洛斯",
-                    "ability": "每个夜晚*，选择两名玩家：他们死亡。死去的玩家可能会复活。",
+                    "ability": "每个夜晚*，选择两名玩家：他们死亡。你上个夜晚选择过且当前死亡的玩家之一可能会被你反刍。",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 27
@@ -417,7 +417,7 @@ SCRIPTS = {
                 {
                     "id": "po",
                     "name": "珀",
-                    "ability": "每个夜晚*，你可以选择一名玩家：他死亡。如果你的上一个夜间选择是没有人，选择三名玩家：他们死亡。",
+                    "ability": "每个夜晚*，你可以选择一名玩家：他死亡。如果你上次选择时没有选择任何玩家，当晚你要选择三名玩家：他们死亡。",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 28
@@ -434,7 +434,7 @@ SCRIPTS = {
                 {
                     "id": "clockmaker",
                     "name": "钟表匠",
-                    "ability": "在你的首个夜晚，你会得知恶魔和最近的爪牙之间相隔多少步。",
+                    "ability": "在你的首个夜晚，你会得知恶魔与爪牙之间最近的距离。（邻座的玩家距离为1）",
                     "first_night": True,
                     "other_nights": False,
                     "night_order": 31
@@ -442,7 +442,7 @@ SCRIPTS = {
                 {
                     "id": "dreamer",
                     "name": "筑梦师",
-                    "ability": "每个夜晚，选择一名玩家（不包括你自己）：你会得知一个善良角色和一个邪恶角色，其中一个是他的角色。",
+                    "ability": "每个夜晚，你要选择除你及旅行者以外的一名玩家：你会得知一个善良角色和一个邪恶角色，该玩家是其中一个角色。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 41
@@ -450,7 +450,7 @@ SCRIPTS = {
                 {
                     "id": "snake_charmer",
                     "name": "舞蛇人",
-                    "ability": "每个夜晚，选择一名存活玩家：一名被选中的恶魔与你互换角色和阵营，然后中毒。",
+                    "ability": "每个夜晚，你要选择一名存活的玩家：如果你选中了恶魔，你和他交换角色和阵营，然后他中毒。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 19
@@ -490,7 +490,7 @@ SCRIPTS = {
                 {
                     "id": "savant",
                     "name": "博学者",
-                    "ability": "每天一次，你可以拜访说书人获得两条信息。其中一条是真的，另一条可能是假的。",
+                    "ability": "每个白天，你可以私下询问说书人以得知两条信息：一个是正确的，一个是错误的。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -498,7 +498,7 @@ SCRIPTS = {
                 {
                     "id": "seamstress",
                     "name": "女裁缝",
-                    "ability": "游戏中仅一次，在夜晚，选择两名玩家（不包括你自己）：你会得知他们是否是同一阵营。",
+                    "ability": "每局游戏限一次，在夜晚时，你可以选择除你以外的两名玩家：你会得知他们是否为同一阵营。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 42
@@ -506,7 +506,7 @@ SCRIPTS = {
                 {
                     "id": "philosopher",
                     "name": "哲学家",
-                    "ability": "游戏中仅一次，在夜晚，选择一个善良角色：获得该角色的能力。如果这个角色在场，他会喝醉。",
+                    "ability": "每局游戏限一次，在夜晚时，你可以选择一个善良角色：你获得该角色的能力。如果这个角色在场，他醉酒。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 1
@@ -514,7 +514,7 @@ SCRIPTS = {
                 {
                     "id": "artist",
                     "name": "艺术家",
-                    "ability": "游戏中仅一次，在白天，你可以私下向说书人问一个是或否的问题。",
+                    "ability": "每局游戏限一次，在白天时，你可以私下询问说书人一个是非问题，你会得知该问题的答案。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -522,7 +522,7 @@ SCRIPTS = {
                 {
                     "id": "juggler",
                     "name": "杂耍艺人",
-                    "ability": "在你的第一个白天，你可以公开猜测最多5名玩家的角色。今晚，你会得知你猜对了多少。",
+                    "ability": "在你的首个白天，你可以公开猜测任意玩家的角色最多五次。在当晚，你会得知猜测正确的角色数量。",
                     "first_night": True,
                     "other_nights": False,
                     "night_order": 59
@@ -530,7 +530,7 @@ SCRIPTS = {
                 {
                     "id": "sage",
                     "name": "贤者",
-                    "ability": "如果恶魔杀死了你，你会得知两名玩家，其中一个是恶魔。",
+                    "ability": "如果恶魔杀死了你，在当晚你会被唤醒并得知两名玩家，其中一名是杀死你的那个恶魔。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 42,
@@ -541,7 +541,7 @@ SCRIPTS = {
                 {
                     "id": "mutant",
                     "name": "畸形秀演员",
-                    "ability": "如果你是外来者被'错误识别'并被处决，你不会死亡，执行者死亡。",
+                    "ability": "如果你“疯狂”地证明自己是外来者，你可能被处决。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -549,7 +549,7 @@ SCRIPTS = {
                 {
                     "id": "sweetheart",
                     "name": "心上人",
-                    "ability": "当你死亡时，一名玩家会喝醉，从现在一直到游戏结束。",
+                    "ability": "当你死亡时，会有一名玩家开始醉酒，从现在一直到游戏结束。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -557,7 +557,7 @@ SCRIPTS = {
                 {
                     "id": "barber",
                     "name": "理发师",
-                    "ability": "如果你在夜晚死亡，恶魔可以选择两名玩家（不是恶魔）来交换角色。",
+                    "ability": "如果你在夜晚死亡，在当晚恶魔可以选择两名玩家（不能选择其他恶魔）交换角色。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 45,
@@ -566,7 +566,7 @@ SCRIPTS = {
                 {
                     "id": "klutz",
                     "name": "呆瓜",
-                    "ability": "当你得知自己死亡时，选择一名存活玩家：如果他是邪恶的，你的阵营输。",
+                    "ability": "当你得知你死亡时，你要公开选择一名存活的玩家：如果他是邪恶的，你的阵营落败。",
                     "first_night": False,
                     "other_nights": False,
                     "night_order": 0
@@ -576,7 +576,7 @@ SCRIPTS = {
                 {
                     "id": "evil_twin",
                     "name": "邪恶双子",
-                    "ability": "你和一名善良玩家都知道对方是谁。如果善良的双胞胎因处决而死亡，邪恶获胜。善良的双胞胎无法死亡，除非你已死亡。",
+                    "ability": "你和一名善良玩家都知道对方是谁。如果善良的双胞胎因处决而死亡，邪恶获胜。如果你们都存活，善良阵营无法获胜。",
                     "first_night": True,
                     "other_nights": False,
                     "night_order": 23
@@ -584,7 +584,7 @@ SCRIPTS = {
                 {
                     "id": "cerenovus",
                     "name": "洗脑师",
-                    "ability": "每个夜晚，选择一名玩家和一个善良角色：他'疯狂'地认为自己是那个角色，否则可能被处决。",
+                    "ability": "每个夜晚，你要选择一名玩家和一个善良角色。他明天白天和夜晚需要“疯狂”地证明自己是这个角色，不然他可能被处决。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 20
@@ -592,7 +592,7 @@ SCRIPTS = {
                 {
                     "id": "pit_hag",
                     "name": "麻脸巫婆",
-                    "ability": "每个夜晚*，选择一名玩家和一个角色：他们会变成那个角色(如果那个角色不在场)，如果因此创造了一个恶魔，当晚的死亡由说书人决定。",
+                    "ability": "每个夜晚*，你要选择一名玩家和一个角色，如果该角色不在场，他变成该角色。如果因此创造了一个恶魔，当晚的死亡由说书人决定。",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 16
@@ -600,7 +600,7 @@ SCRIPTS = {
                 {
                     "id": "witch",
                     "name": "女巫",
-                    "ability": "每个夜晚，选择一名玩家：如果他明天提名，他会立即死亡。",
+                    "ability": "每个夜晚，你要选择一名玩家：如果他明天白天发起提名，他死亡。如果只有三名存活的玩家，你失去此能力。",
                     "first_night": True,
                     "other_nights": True,
                     "night_order": 14
@@ -610,7 +610,7 @@ SCRIPTS = {
                 {
                     "id": "fang_gu",
                     "name": "方古",
-                    "ability": "每个夜晚*，选择一名玩家：他死亡。你第一次选择外来者时，你死亡，他成为方骨并立即中毒。",
+                    "ability": "每个夜晚*，选择一名玩家：他死亡。被该能力杀死的外来者改为变成邪恶的方古且你代替他死亡，但每局游戏仅能成功转化一次。[+1外来者]",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 29
@@ -618,15 +618,15 @@ SCRIPTS = {
                 {
                     "id": "vigormortis",
                     "name": "亡骨魔",
-                    "ability": "每个夜晚*，选择一名玩家：他死亡。爪牙死亡后保留能力。如果爪牙因你而死，今晚杀死他的一个镇民邻居。",
+                    "ability": "每个夜晚*，选择一名玩家：他死亡。被你杀死的爪牙保留他的能力，且与他邻近的两名镇民之一中毒。[-1外来者]",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 30
                 },
                 {
                     "id": "no_dashii",
-                    "name": "诺达虱",
-                    "ability": "每个夜晚*，选择一名玩家：他死亡。你最近的两个镇民邻居中毒。",
+                    "name": "诺-达鲺",
+                    "ability": "每个夜晚*，选择一名玩家：他死亡。与你邻近的两名镇民中毒。",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 31
@@ -634,7 +634,7 @@ SCRIPTS = {
                 {
                     "id": "vortox",
                     "name": "涡流",
-                    "ability": "每个夜晚*，选择一名玩家：他死亡。镇民的能力给出的信息都是假的。每天，如果没有人被处决，邪恶获胜。",
+                    "ability": "每个夜晚*，选择一名玩家：他死亡。镇民玩家的能力都会产生错误信息。如果白天没人被处决，邪恶阵营获胜。",
                     "first_night": False,
                     "other_nights": True,
                     "night_order": 32
